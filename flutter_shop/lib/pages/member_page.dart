609055c5ddb85235e56c50e8fd9d9c27a95d06c6
HeYaFei <heyafei12138@163.com>
2020-11-06 18:07:30 +0800

    个人中心

--- flutter_shop/lib/pages/member_page.dart
+++ flutter_shop/lib/pages/member_page.dart
@@ -1,15 +1,66 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
 
 class MemberPage extends StatelessWidget {
-  const MemberPage({Key key}) : super(key: key);
-
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Center(
-        child: Text('这是个人中心'),
+      appBar: AppBar(
+        title: Text('会员中心'),
       ),
-      backgroundColor: Colors.pink,
+      body: ListView(
+        children: [
+          _topHeader(),
+          Container(
+            height: 5,
+            color: Colors.black12,
+          ),
+          _orderTitle(),
+        ],
+      ),
+    );
+  }
+
+  Widget _topHeader() {
+    return Container(
+      width: ScreenUtil().setWidth(750),
+      padding: EdgeInsets.all(20),
+      color: Colors.pinkAccent,
+      child: Column(
+        children: [
+          Container(
+            margin: EdgeInsets.only(top: ScreenUtil().setHeight(30)),
+            child: ClipOval(
+              child: Image.network(
+                'https://wx3.sinaimg.cn/large/005P9ygYly1gkfdehylcbj30dw0dwq3v.jpg',
+                height: 180,
+              ),
+            ),
+          ),
+          Container(
+            margin: EdgeInsets.only(top: 10),
+            child: Text(
+              '飞是飞的飞',
+              style: TextStyle(
+                  fontSize: ScreenUtil().setSp(40), color: Colors.blue),
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+
+  Widget _orderTitle() {
+    return Container(
+      margin: EdgeInsets.only(top: 10),
+      decoration: BoxDecoration(
+          color: Colors.white,
+          border: Border(bottom: BorderSide(width: 1, color: Colors.black38))),
+      child: ListTile(
+        leading: Icon(Icons.list),
+        title: Text('我的订单'),
+        trailing: Icon(Icons.arrow_right),
+      ),
     );
   }
 }
